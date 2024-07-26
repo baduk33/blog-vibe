0dc8b776bc01a3bd2b4a055b3d654b68e2eac66d
charanpreet01 <charanpreetsinghrajput@gmail.com>
2024-07-26 19:58:01 +0530

    installing react and tailwind css

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold text-center text-fuchsia-500 my-5'>hello</h1>
+    </>
+  )
+}
+
+export default App
