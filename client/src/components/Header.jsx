8c1adcbff0bf1951a338fffea776db271d01c9cb
charanpreet01 <charanpreetsinghrajput@gmail.com>
2024-07-27 00:31:49 +0530

    create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -30,7 +30,7 @@ function Header() {
                     <FaMoon />
                 </Button>
                 <Link to='/sign-in'>
-                    <Button gradientDuoTone="purpleToBlue">Sign In</Button>
+                    <Button gradientDuoTone="greenToBlue" outline>Sign In</Button>
                 </Link>
                 <Navbar.Toggle />
             </div>
