367e74f11a5579a26c7680beea30503a61961ba7
charanpreet01 <charanpreetsinghrajput@gmail.com>
2024-07-27 22:03:59 +0530

    create a sign up page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -30,7 +30,7 @@ function Header() {
                     <FaMoon />
                 </Button>
                 <Link to='/sign-in'>
-                    <Button gradientDuoTone="greenToBlue" outline>Sign In</Button>
+                    <Button gradientDuoTone="purpleToBlue" outline>Sign In</Button>
                 </Link>
                 <Navbar.Toggle />
             </div>
