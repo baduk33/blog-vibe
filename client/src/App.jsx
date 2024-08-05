8b258570783c7cc5769feaf18a7bff097b8e7c51
charanpreet01 <charanpreetsinghrajput@gmail.com>
2024-08-05 22:00:00 +0530

    complete post page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,30 +12,29 @@ import PrivateRoute from "./components/PrivateRoute"
 import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute"
 import CreatePost from "./pages/CreatePost"
 import UpdatePost from "./pages/UpdatePost"
-import AutoLogoutProvider from "./components/AutoLogoutProvider"
+import PostPage from "./pages/PostPage"
 
 function App() {
 
   return (
     <BrowserRouter>
-      <AutoLogoutProvider>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route element={<PrivateRoute />} >
-            <Route path="/dashboard" element={<Dashboard />} />
-          </Route>
-          <Route element={<OnlyAdminPrivateRoute />} >
-            <Route path="/create-post" element={<CreatePost />} />
-            <Route path="/update-post/:postId" element={<UpdatePost />} />
-          </Route>
-          <Route path="/sign-in" element={<Signin />} />
-          <Route path="/sign-up" element={<Signup />} />
-        </Routes>
-        <Footer />
-      </AutoLogoutProvider>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route element={<PrivateRoute />} >
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+        <Route element={<OnlyAdminPrivateRoute />} >
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:postId" element={<UpdatePost />} />
+        </Route>
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/post/:postSlug" element={<PostPage />} />
+      </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
