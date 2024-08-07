6b1b20d7bde19755f0a6677f90548fcd8f0549b2
charanpreet01 <charanpreetsinghrajput@gmail.com>
2024-08-07 05:30:23 +0530

    complete about and projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,24 @@ import React from 'react'
 
 function About() {
   return (
-    <div>
-      about
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div className="">
+          <h1 className='text-3xl font-semibold text-center mb-7'>About BlogVibe</h1>
+          <div className="text-md text-gray-500 flex flex-col gap-5">
+            <p>
+              Welcome to BlogVibe! This is your go-to destination for engaging and insightful content, crafted with passion and creativity.
+              BlogVibe offers a seamless and dynamic user experience.
+            </p>
+            <p>
+              Here, we dive deep into the worlds of web development, software engineering, and programming languages. Our mission is to provide you with insightful articles, tutorials, and tips to enhance your skills and knowledge. Join us on this journey of learning and innovation, and let's build something amazing together.
+            </p>
+            <p>
+              Dive in and explore a variety of topics, all brought to you with a unique vibe that sets us apart. Join our community and let's vibe together!
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
